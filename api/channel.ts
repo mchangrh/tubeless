34ede37d24ec17e66f259653b1e4ed81c54f8bf1
Michael C <michael@mchang.name>
2022-10-14 03:21:35 -0400

    add support for repeating arguments

--- api/channel.ts
+++ api/channel.ts
@@ -14,24 +14,36 @@ const makeBody = (videoID: string) => JSON.stringify(
   }
 )
 
+const getDetails = (videoID: string) =>
+  fetch(itURL, {
+    method: 'POST',
+    body: makeBody(videoID)
+  })
+  .then(res => res.json())
+  .then(json => {
+    const details = json.videoDetails
+    return {
+      videoId: details.videoId,
+      title: details.title,
+      channelId: details.channelId,
+      channelName: details.author,
+    }
+  })
+
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
-  const { videoID } = request.query;
+  const { videoID, videoIDs } = request.query;
+  if (Array.isArray(videoIDs)) {
+    const details = await Promise.all(
+      videoIDs.map((id: string) => getDetails(id))
+    )
+    response.status(200).json(details)
+  }
   if (videoID?.length !== 11) return response.status(400).send("No videoID provided")
-  const res = await fetch(itURL, {
-    method: 'POST',
-    body: makeBody(videoID as string)
-  })
-  const data = await res.json()
-  const details = data.videoDetails
+  const results = getDetails(videoID as string)
   return response.status(200)
-  .setHeader("Content-Type", "application/json")
-  .json({
-    videoId: details.videoId,
-    title: details.title,
-    channelId: details.channelId,
-    channelName: details.author,
-  })
+    .setHeader("Content-Type", "application/json")
+    .json(results)
 }
